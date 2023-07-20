bdc5b2392d2aae905f35c1c156ccec6c71f41f73
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-20 18:47:10 +0300

    added workoutSubController

---
+++ server/controllers/subController/workoutSubController.js
@@ -0,0 +1,27 @@
+// IMPORTING NECESSARY MIDDLEWARE
+const eventLogger = require('../middleware/eventLogger')
+
+//DEFINING A GETCONTROLLER FUNCTION THAT HANDLES GET REQUESTS
+function getController(req, res, next){
+    res.send("Hello")
+    next()
+}
+
+//DEFINING A DELETECONTROLLER FUNCTION THAT HANDLES DELETE REQUESTS
+function deleteController(req, res, next){
+    res.send("Hello")
+    next()
+}
+
+//DEFINING A PATCHCONTROLLER FUNCTION THAT HANDLES PATCH REQUESTS
+function patchController(req, res, next){
+    res.send("Hello")
+    next()
+}
+
+// EXPORTING VARIOUS CONTROLLERS
+module.exports = { 
+    getController,
+    deleteController,
+    patchController
+}
--- server/routers/subrouters/workoutSubRouter.js
+++ server/routers/subrouters/workoutSubRouter.js
@@ -1,11 +1,15 @@
 // IMPORTING NECESSARY LIBRARIES AND MIDDLEWARE
 const express = require('express')
+const workoutSubController = require('../../controllers/subController/workoutSubController')
 
 // INITIALIZE EXPRESS ROUTER
 const workoutSubRouter = express.Router()
 
 // ROUTE MIDDLEWARES
-workoutSubRouter.get("/:id", workoutSubController.getController)
+workoutSubRouter.route("/:id")
+    .get(workoutSubController.getController)
+    .delete(workoutSubController.deleteController)
+    .patch(workoutSubController.patchController)
 
 // EXPORT WORKOUTSUBROUTER
 module.exports = workoutSubRouter
