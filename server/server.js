bac327ac0b8d14f464dd5311a5837675017ec2fc
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-20 15:17:43 +0300

    added server file

--- server/server.js
+++ server/server.js
@@ -1,3 +1,17 @@
-const eventLogger = require("./middleware/eventLogger");
+// IMPORTING NECESSARY LIBRARIES AND MIDDLEWARES
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const eventLogger = require('./middleware/eventLogger')
+const rootRouter = require('./routers/rootRouter')
 
-eventLogger()
+// INITIATING EXPRESS APP WITH OTHER LIBRARIES
+const app = express()
+dotenv.config()
+
+// NON-ROUTE MIDDLEWARES
+app.use(cors())
+app.use(express.json())
+
+// ROUTE MIDDLEWARES
+app.use("/", rootRouter)
