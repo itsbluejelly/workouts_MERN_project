35dcc357c406bf2564cc5803dd58c7f3a6b3016f
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-20 15:18:14 +0300

    added rootrouter

---
+++ server/routers/rootRouter.js
@@ -0,0 +1,12 @@
+// IMPORT NECESSARY LIBRARIES AND MIDDLEWARE
+const express = require('express')
+const rootController = require('../controllers/rootController')
+
+// INITIALIZING EXPRESS ROUTER
+const rootRouter = express.Router()
+
+// ROUTE MIDDLEWARES
+rootRouter.get("/", rootController)
+
+// EXPORTING ROOTROUTER
+module.exports = rootRouter
