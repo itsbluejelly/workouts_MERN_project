394c58495021e9be983d1f18b26b51c8d7f0b2e1
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-24 13:15:01 +0300

    Adde formatted recent dates

--- client/src/components/Workout.jsx
+++ client/src/components/Workout.jsx
@@ -1,10 +1,12 @@
+import format from 'date-fns/formatDistanceToNow'
+
 export default function Workout(props){
     return(
         <div className="workout-details">
             <h4>{props.workout.title}</h4>
             <p><strong>Load (kg): </strong>{props.workout.load}</p>
             <p><strong>Reps (kg): </strong>{props.workout.reps}</p>
-            <p>{props.workout.createdAt}</p>
+            <p>{format(new Date(props.workout.updatedAt), {addSuffix: true})}</p>
             <span onClick={props.handleClick}>&#128465;</span>
         </div>
     )
