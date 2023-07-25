d50d4eeb33c31432ff0c5ea00eafd52da6149c73
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-25 15:28:51 +0300

    added login functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,8 +6,43 @@ const UserModel = require('../models/User')
 const JWTGENERATOR = require('../middleware/JWTGenerator')
 
 // A LOGINPOSTCONTROLLER TO DEAL WITH POST REQUESTS
-function loginPostController(req, res, next){
-    res.send("Hi")
+async function loginPostController(req, res, next){
+    const {username, email, password} = req.body
+
+    try{
+        if(!username){
+            throw new Error("You must provide a username")
+        }else if(!email){
+            throw new Error("You must provide an email")
+        }else if(password.length < 5){
+            throw new Error("Your password must have a minimum of 5 characters")
+        }else{
+            const foundUser = await UserModel.findOne({email})
+
+            if(!foundUser){
+                throw new Error("Invalid Email")
+            }else{
+                const validatedPassword = bcrypt.compare(password, foundUser.password)
+                
+                if(!validatedPassword){
+                    throw new Error("Incorrect password")
+                }else{
+                    const token = JWTGENERATOR({id: foundUser._id}, process.env.SECRET_KEY, '24h')
+                    
+                    res.status(200).json({ 
+                        success: `User ${foundUser.username} successfully logged in`,
+                        token: token
+                    })
+                    
+                    eventLogger(`User with id ${foundUser._id} successfully logged in`, foundUser, "UserLogs.txt")
+                }
+            }
+        }
+    }catch(error){
+        res.status(401).json({ error: error.message })
+        eventLogger(error.name, error.message, "errorLogs.txt")
+    }
+
     next()
 }
 
@@ -36,10 +71,12 @@ async function signupPostController(req, res, next){
                 const hashedPassword = await bcrypt.hash(password, saltRounds)
                 const createdUser = await UserModel.create({username, email, password: hashedPassword})
                 const token = JWTGENERATOR({id: createdUser._id}, process.env.SECRET_KEY, '24h')
+                
                 res.status(201).json({
                     success: `${createdUser.username}'s account created successfully`,
                     token: token
                 })
+                
                 eventLogger(`User with id ${createdUser._id} successfully created`, createdUser, "UserLogs.txt")
             }   
         }
